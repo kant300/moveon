a700905a9d897d97d7f8775654065618a15dad2e
rutaku12rutaku12 <chookyuho@gmail.com>
2025-09-01 18:05:14 +0900

    250901_쓰레기 jsp파일 작성중

---
+++ src/main/resources/static/js/living/trash/trash.js
@@ -0,0 +1,14 @@
+
+console.log('trash.js open');
+
+// [*] 썸머노트 실행
+$(document).ready(function() {
+            $('#summernote').summernote({
+                lang: 'ko-KR' // 한국어 설정
+            });
+        });
+
+// [1] 쓰레기 배출정보 등록
+const trashAdd() = async() =>{
+    // 1. 첨부파일 있는 게시물 : multipart
+}
