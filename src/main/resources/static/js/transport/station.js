074b9546271a9b7a2f0f8e053638179bca0f9d6e
IceP0tato <rlawodud0313@gmail.com>
2025-09-04 15:20:10 +0900

    250904_map 통합

--- src/main/resources/static/js/transport/station.js
+++ src/main/resources/static/js/transport/station.js
@@ -57,11 +57,23 @@
 const createMap = async () => {
     var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
     mapOption = { 
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        center: new kakao.maps.LatLng(37.4903117, 126.7226046), // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨
     };
 
     var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+    // 데이터를 가져와 필요한 데이터를 삽입합니다
+    /*const data = getData();
+    var positions = [];
+    for (let i=0; i<data.length; i++) {
+        const obj = data[i];
+        const 역사 = obj.get("역사");
+        positions.push({title: 역사, latlng: new kakao.maps.LatLng()});
+    }*/
+
+
+
     
     // 마커를 표시할 위치와 title 객체 배열입니다 
     var positions = [
@@ -110,4 +122,6 @@ const getData = async () => {
     const response = await fetch("/station/data");
     const data = await response.json();
     console.log(data);
+
+    return data;
 }
--- src/main/webapp/map.jsp
+++ src/main/webapp/map.jsp
@@ -3,16 +3,15 @@
 <html>
 <head>
     <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Page Title</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
 </head>
 <body>
     <jsp:include page="/header.jsp"></jsp:include>
 
+    <!-- 지도 -->
     <div id="map" style="width:420px;height:790px;"></div>
-
+    
     <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=473cf3e9725a8ab5ab5f7b3c73d0a328&libraries=services"></script>
-    <script src="/js/transport/station.js"></script>
+    <script src="js/map.js"></script>
 </body>
 </html>
