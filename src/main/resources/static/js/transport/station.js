b779c1b1e3b3f23abecbdf3681cd907fc5554e21
IceP0tato <rlawodud0313@gmail.com>
2025-09-05 10:09:36 +0900

    250905_운행 상태에 따른 마커 이미지 변동 기능 추가

--- src/main/resources/static/js/transport/station.js
+++ src/main/resources/static/js/transport/station.js
@@ -23,15 +23,15 @@ const createMap = async () => {
             status: obj.상태
         });
     }
-
-    // 마커 이미지의 이미지 주소입니다
-    var imageSrc = "/img/마커.png";
         
     for (let i = 0; i < positions.length; i ++) {
         const obj = positions[i];
 
+        // 마커 이미지의 이미지 주소입니다
+        var imageSrc = obj.status=="수리중"?"/img/마커_red.png":"/img/마커.png";
         // 마커 이미지의 이미지 크기 입니다
-        var imageSize = new kakao.maps.Size(24, 35); 
+        var imageSize = new kakao.maps.Size(25, 34);
+        // var imageOption = {offset: new kakao.maps.Point(32, 9)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
         
         // 마커 이미지를 생성합니다    
         var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
