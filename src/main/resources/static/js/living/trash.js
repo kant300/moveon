9752f70e37d17551d80a4d3eb0fad863274b4d70
itdanjalog <kgs2072@naver.com>
2025-09-05 10:40:57 +0900

    test

--- src/main/resources/static/js/living/trash.js
+++ src/main/resources/static/js/living/trash.js
@@ -4,48 +4,55 @@ console.log('trash.js open');
 
 // [1] 카카오맵 API을 이용하여 현재 위치좌표 => 현재 위치 지역 시,구 정보얻기 
 
-    // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
-    if (navigator.geolocation) {
-        // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var lat = position.coords.latitude, // 위도
-                lon = position.coords.longitude; // 경도
-                console.log(lat,lon);
-                geocoder.coord2RegionCode(lon , lat, callback);
-        });
-    } 
+// HTML5의 geolocation으로 사용할 수 있는지 확인합니다
+if (navigator.geolocation) {
+    // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var lat = position.coords.latitude, // 위도
+            lon = position.coords.longitude; // 경도
+            console.log(lat,lon);
+            // geocoder.coord2RegionCode(lon , lat, callback);
+            geocoder.coord2RegionCode( 126.723325411  , 37.489572382, callback); // 부평역 테스트
+    });
+}
 // [2] 좌표를 주소로 변환, 화면에 출력
 var geocoder = new kakao.maps.services.Geocoder();
 var callback = function(result, status) {
     if (status === kakao.maps.services.Status.OK) {
+
         console.log(`지역 명칭 : ${result[0].region_1depth_name} ${result[0].region_2depth_name}`); 
         // region_1depth_name : 시도 단위 , region_2depth_name : 구 단위
         // https://developers.kakao.com/docs/latest/ko/local/dev-guide#coord-to-district // 추후 커스텀시 지역정보코드모음
         
         // textBox(jsp)에 현재 접속한 (시도단위, 구단위)위치 출력 
         document.querySelector('#textBox').innerHTML = `현재 접속 위치는 <strong>${result[0].region_1depth_name} ${result[0].region_2depth_name}</strong> 입니다.`;
+
+        const 지역_시도 = result[0].region_1depth_name;
+        console.log( 지역_시도 );   // 경기도 // 인천광역시
+
+        const 지역_구 = result[0].region_2depth_name;
+        console.log( 지역_구 ); // 부천시 원미구 //
+
+        // if 로 광역시 와 도(8) 구분해서
+
+
+        trashFind( 지역_시도+" "+지역_구.split(" ")[0]  , 지역_구.split(" ")[1]  ); // : 쓰레기 개별 배출정보 호출 // 등록시 지역명들을  select 고민!!
+
     }
     
 };
 
 // [3] 쓰레기 개별 배출정보 출력
-
-// [*] 현재 URL 경로상의 tno 매개변수값 가져오기 ( searchParams == queryString == ?매개변수명=값 )
-const params = new URL(location.href).searchParams;
-const tcity = params.get('tCity');
-const tgu = params.get('tGu');
-
-const trashFind = async () =>{
+const trashFind = async ( tCity , tGu ) =>{
     try{ // 1. 어디에 // fetch로 부터 출력할 쓰레기 정보 조회 요청 
-        const response = await fetch(`/living/trash/find?tCity=${tcity}&tGu=${tgu}`); // GET => 옵션생략
+        const response = await fetch(`/living/trash/find?tCity=${tCity}&tGu=${tGu}`); // GET => 옵션생략
         const data = await response.json();
         // 2 무엇을 // 응답받은 자료를 특정한 html에 출력한다.
         document.querySelector('#infoBox').innerHTML = data.tinfo;
 
-        
     }catch(e){console.log(e)};
 }
-trashFind();
+
 
 
 
