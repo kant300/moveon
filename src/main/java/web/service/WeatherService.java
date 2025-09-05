dffeaa1e1b5762b5c4c2823cd0d83c0d99cd6f69
IceP0tato <rlawodud0313@gmail.com>
2025-09-05 18:16:34 +0900

    250905_기상청 단기예보 API 테스트

--- src/main/java/web/service/WeatherService.java
+++ src/main/java/web/service/WeatherService.java
@@ -26,6 +26,7 @@ public class WeatherService {
             DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyyMMdd");
             // 현재 시간, 시 단위 (ex : 1200, 0600)
             DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern(now.getHour()>=10?"HH00":"0H00");
+            // now.format(timeFormat)
 
             // 기상청 API 주소
             StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst"); /*URL*/
@@ -34,7 +35,7 @@ public class WeatherService {
             urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
             urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON) Default: XML*/
             urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode(now.format(dateFormat), "UTF-8")); /*‘21년 6월 28일 발표*/
-            urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode(now.format(timeFormat), "UTF-8")); /*06시 발표(정시단위) */
+            urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode("0600", "UTF-8")); /*06시 발표(정시단위) */
             // TODO: 예보지점의 x, y 좌표값은 JS 의 Geolocation 을 통해 얻은 위도, 경도 좌표값을 대입해야 함 (현재 위치에 따른 기상 정보 요청)
             urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode("55", "UTF-8")); /*예보지점의 X 좌표값*/
             urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode("123", "UTF-8")); /*예보지점의 Y 좌표값*/
@@ -59,9 +60,13 @@ public class WeatherService {
                 sb.append(line);
             }
 
-            Map<String, String> map = new LinkedHashMap<>();
             JSONObject obj = new JSONObject(rd);
-            JSONArray array = obj.getJSONObject("items").getJSONArray("item");
+            System.out.println(obj);
+
+
+            /*Map<String, String> map = new LinkedHashMap<>();
+            JSONObject obj = new JSONObject(rd);
+            JSONArray array = obj.getJSONObject("response").getJSONObject("body").getJSONObject("items").getJSONArray("item");
 
             String baseDate = array.getJSONObject(0).getString("baseDate");
             String baseTime = array.getJSONObject(0).getString("baseTime");
@@ -73,7 +78,7 @@ public class WeatherService {
             System.out.println(baseTime);
             System.out.println(nx);
             System.out.println(ny);
-            System.out.println(category);
+            System.out.println(category);*/
 
 
             rd.close();
